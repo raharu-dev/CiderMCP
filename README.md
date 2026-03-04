6238314dc6d277fe1109094c5f6be7d52f5349e7
Adam <adamek.wola@gmail.com>
2026-03-04 21:48:13 +0100

    Barebone MCP implementation

--- README.md
+++ README.md
@@ -4,6 +4,47 @@ This is a project to allow LLMs connect to Cider.sh music app.
 I don't really have a big experience in Python or MCP so this project is going to be a nice study.
 Overall I will be guiding myself by using the repo below.
 So this repo will be my personal journal of my journey with Python and MCP.
-The MCP will be tested using local instance of Qwen3.5 9B
+The MCP will be tested using local instance of Qwen3.5 9B in LM Studio.
+## Planned MCP Tools
+- Play, Pause
+- Set Volume
+- Shuffle, Loop, Autoplay
+
+- Get Queue
+- Move Queue Position
+- Remove from Queue
+- Clear Queue
+
+- Add Song/Album (URL/HREF/ID)
+- Get Lyrics
+- Search (/api/v1/amapi)
+## How to install
+Github version:
+```json
+{
+  "mcpServers": {
+    "cider-music": {
+      "command": "uv",
+      "args": [
+        "--directory",
+        "/Path/To/Cider3MCP",
+        "run",
+        "main.py"
+      ],
+      "env": {
+        "CIDER_TOKEN": "CIDER-API-TOKEN",
+        "CIDER_PORT": "10767"
+      }
+    }
+  }
+}
+```
 ## Sources
-https://github.com/modelcontextprotocol/python-sdk
+MCP Python SDK:
+ https://github.com/modelcontextprotocol/python-sdk
+
+Cider API:
+ https://cider.sh/docs/client/rpc
+
+This helped me how to add to mcp.json locally:
+ https://github.com/reading-plus-ai/mcp-server-deep-research
--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-def main():
-    print("Hello from cider3mcp!")
+"""
+MCP server to use Cider music player with LLMs
+"""
 
+# Imports
+import os
+import requests
+from mcp.server.fastmcp import FastMCP
+
+# Get env arguments
+CIDER_TOKEN=os.environ.get("CIDER_TOKEN")
+if not CIDER_TOKEN:
+    CIDER_TOKEN=""
+CIDER_PORT=os.environ.get("CIDER_PORT")
+if not CIDER_PORT:
+    CIDER_PORT="10767"
+
+
+API_URL="http://localhost:"+CIDER_PORT+"/api/v1/"
+TOKEN_HEADER = {
+    # Key : Value
+    "apptoken": CIDER_TOKEN
+}
+
+# Create MCP server
+mcp = FastMCP("Cider-Music", json_response=True)
+
+# Check Cider status and playback
+@mcp.resource()
+def check_status():
+    """Check if Cider music player is open and its playback status"""
+    response = requests.get(API_URL + "playback/is-playing", headers=TOKEN_HEADER)
+    if response.status_code == 200:
+        # If the request was successful, return the status and whether music is playing
+        return {"status": "Cider is working.", "is_playing": response.json().get("is_playing")}
+    else:
+        # Error handling to know if token is wrong or if Cider is not open
+        if response.json() == {"error":"UNAUTHORIZED_APP_TOKEN"}:
+            return {"error": "Invalid Cider token. Please check your CIDER_TOKEN environment variable."}
+        else:
+            return {"error": "Cider is not open or not responding. Check if Cider have Web API enabled and if the port is correct."}
 
 if __name__ == "__main__":
-    main()
-"""
-asda
-dsad
-"""
+    # mcp.run(transport="streamable-http")
+    mcp.run()
